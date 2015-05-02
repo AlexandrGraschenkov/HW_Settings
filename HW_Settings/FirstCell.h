be4a730e9ddd266e30725e4859ead4571cfa2f28
Bazinko <Zik-zhenya@mail.ru>
2015-05-02 14:08:38 +0300

    - init

---
+++ HW_Settings/FirstCell.h
@@ -0,0 +1,15 @@
+//
+//  BaseCell.h
+//  HW_Settings
+//
+//  Created by Евгений Сергеев on 17.03.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *title;
+
+@end
---
+++ HW_Settings/SecondCell.m
@@ -0,0 +1,14 @@
+//
+//  DetailCell.m
+//  HW_Settings
+//
+//  Created by Евгений Сергеев on 17.03.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import "SecondCell.h"
+
+@implementation SecondCell
+
+
+@end
